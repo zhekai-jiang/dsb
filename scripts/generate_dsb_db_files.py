97a7ac2f56c841442c40c0063548118bc57767bb
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2025-05-14 13:30:33 +0200

    Fix data generation script with Linux syntax

--- scripts/generate_dsb_db_files.py
+++ scripts/generate_dsb_db_files.py
@@ -2,11 +2,11 @@ import os
 import subprocess
 import shutil
 
-bin_dir = r'D:\code\v2.11.0rc2\tools'
-bin_path = os.path.join(bin_dir, 'dsdgen.exe')
+bin_dir = r'/home/jzk/benchmarks/dsb/code/tools'
+bin_path = os.path.join(bin_dir, 'dsdgen')
 
-scale = 10
-file_dir = # directory of database files
+scale = 1
+file_dir = r'/home/jzk/benchmarks/dsb/data' # directory of database files
 
 if os.path.exists(file_dir):
 	shutil.rmtree(file_dir)
@@ -16,6 +16,6 @@ if not os.path.exists(file_dir):
 
 # WARNING: This code hasn't been valiated. Somewhat the dir parameter is not
 # taken in to account by the code. The output db files are generated in bin_dir
-cmd = [bin_path, '/scale', str(scale), '/terminate', 'N', '/force', '/dir',  file_dir]
+cmd = [bin_path, '-scale', str(scale), '-terminate', 'N', '-force', '-dir',  file_dir]
 print(cmd)
 subprocess.call(cmd, cwd = bin_dir)
